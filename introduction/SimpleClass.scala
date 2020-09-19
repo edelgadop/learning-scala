7d9830a19148152f529f33d736e2eaf215ea728d
edelgadop <33735338+edelgadop@users.noreply.github.com>
2020-09-19 15:09:47 +0200

    A Simple Scala Class

---
+++ introduction/SimpleClass.scala
@@ -0,0 +1,30 @@
+package introduction
+
+class SimpleClass {
+  private var _x: Int = 0
+  private var _y: Int = 0
+
+  def x = _x
+  def x_= (newValue: Int) = {
+    _x = newValue
+  }
+
+  def y = _y
+  def y_= (newValue: Int): Unit = {
+    _y = newValue
+  }
+
+  def sum: Int = _x + _y
+  def difference: Int = _x - _y
+  def product: Int = _x * _y
+
+  override def toString: String = "x = " + _x + "\n" + "y = " + _y + "\n" + "x + y = " + sum + "\n" + "x - y = " +
+    difference + "\n" + "x * y = " + product
+}
+
+object Main extends App {
+  var simpClass = new SimpleClass
+  simpClass.x = 10
+  simpClass.y = 5
+  print(simpClass)
+}
